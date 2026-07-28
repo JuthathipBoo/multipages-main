c7950acdc7b1a74de7d3f6e32e1229092fc602c6
JuthathipBoo <juthathip.bon@spumail.net>
2026-07-28 22:29:38 +0700

    initial commit

---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,89 @@
+// import { func } from "prop-types";
+import "./Timer.css";
+import { useEffect, useState } from "react";
+
+function Timer() {
+  const [running, setRunning] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+          let interval = null;
+          if(running){
+                    interval = setInterval(() => {
+                              setSeconds(seconds + 1)
+                    },1000)
+          }
+          return() => clearInterval(interval)
+  },[running,seconds])
+
+  function runClick() {
+    setRunning(!running);
+  }
+
+  // function secondsToString(seconds) {
+  //         const Minute_seconds = 60;
+  //         const Hour_seconds = 60 * Minute_seconds;
+  //         const Day_seconds = 24 * Hour_seconds;
+
+  //         const days = Math.floor(seconds / Day_seconds);
+  //         const hours = Math.floor((seconds % Day_seconds) / Hour_seconds);
+  //         const minutes = Math.floor((seconds % Hour_seconds) / Minute_seconds);
+  //         const secs = seconds % Minute_seconds;
+
+  //         if(days > 0){
+  //                   return `${days}d ${hours}h ${minutes}m ${secs}s`;
+  //         }else if(hours > 0){
+  //                   return `${hours}h ${minutes}m ${secs}s`;
+  //         }else if(minutes > 0){
+  //                   return `${minutes}m ${secs}s`;
+  //         }else{
+  //                   return `${secs}s`;
+  //         }
+  // }
+
+  function secondsToString(seconds) {
+
+    const day = Math.floor(seconds / 86400);
+
+    const hour = Math.floor((seconds % 86400) / 3600);
+
+    const minute = Math.floor((seconds % 3600) / 60);
+
+    const second = seconds % 60;
+
+    return `${day} : ${hour.toString().padStart(2, "0")} : ${minute
+        .toString()
+        .padStart(2, "0")} : ${second.toString().padStart(2, "0")}`;
+}
+
+  function resetClick() {
+          setRunning(false)
+          setSeconds(0)
+  }
+  
+
+  return (
+    <div className="timer-container">
+      <h3 className="timer-title">Timer</h3>
+      <p>
+        <input
+          className="timer-input"
+          type="text"
+          readOnly={true}
+          value={secondsToString(seconds)}
+        />
+      </p>
+      <div className="timer-buttons">
+        <button className="btn btn-danger" onClick={resetClick}>Reset</button>
+        <button
+          className={"btn" + (running ? " btn-warning" : " btn-success")}
+          onClick={runClick}
+        >
+          {running ? "Pause" : "Run"}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Timer;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,99 @@
+import { useRef } from "react";
+import Form from "react-bootstrap/Form";
+import { verifyUser } from "../../data/users";
+import PropTypes from "prop-types";
+import imglog from "../../assets/Coding.png";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+
+import "./Login.css";
+
+function Login({ setToken, setRole }) {
+  const userRef = useRef();
+  const passRef = useRef();
+
+  return (
+    <div className="login-container">
+      <div className="login-img">
+        <img src={imglog} style={{ width: "100%", height: "100%" }} />
+      </div>
+      <br></br>
+      <div className="login-form">
+        <Form.Label htmlFor="username">Username</Form.Label>
+        {/* <Form.Control
+          type="text"
+          id="username"
+          placeholder="user"
+          style={{ textAlign: "left" }}
+          ref={userRef}
+          
+        /> */}
+        <OverlayTrigger
+          placement="top" // ปรับตำแหน่งได้: top, bottom, left, right
+          overlay={
+            <Tooltip id="username-tooltip">type &apos; user &apos;</Tooltip>
+          }
+        >
+          <Form.Control
+            type="text"
+            id="username"
+            placeholder="user"
+            style={{ textAlign: "left" }}
+            ref={userRef}
+          />
+        </OverlayTrigger>
+        <Form.Label htmlFor="password">Password</Form.Label>
+        {/* <Form.Control
+          type="password"
+          id="password"
+          placeholder="pass"
+          style={{ textAlign: "left" }}
+          ref={passRef}
+        /> */}
+        <OverlayTrigger
+          placement="top"
+          overlay={
+            <Tooltip id="password-tooltip">
+              type &apos; pass &apos;
+            </Tooltip>
+          }
+        >
+          <Form.Control
+            type="password"
+            id="password"
+            placeholder="pass"
+            style={{ textAlign: "left" }}
+            ref={passRef}
+          />
+        </OverlayTrigger>
+      </div>
+      <button
+        className="btn btn-success mt-3"
+        style={{ textAlign: "center" }}
+        onClick={() => {
+          const user = userRef.current.value.trim();
+          const pass = passRef.current.value.trim();
+          const userInfo = verifyUser(user, pass);
+          userRef.current.value = "";
+          passRef.current.value = "";
+
+          if (userInfo === null) {
+            alert("Wrong Username or password");
+            useRef.current.focus();
+          } else {
+            setToken(userInfo.token);
+            setRole(userInfo.role);
+          }
+        }}
+      >
+        Login
+      </button>
+    </div>
+  );
+}
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired,
+  setRole: PropTypes.func.isRequired,
+};
+
+export default Login;
